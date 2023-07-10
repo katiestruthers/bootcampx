540af739ecb9286847d3195f443ae84042c001d0
Katherine Struthers <ktstruthers@gmail.com>
2023-07-09 21:24:47 -0500

    create JS app using node-postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${cohortName}%'
+LIMIT ${limit};
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  });
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher,
+      cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${cohortName}%'
+ORDER BY teachers.name;
+`)
+  .then(res => {
+    res.rows.forEach(teacher => {
+      console.log(`${cohortName}: ${teacher.teacher}`);
+    });
+  });
